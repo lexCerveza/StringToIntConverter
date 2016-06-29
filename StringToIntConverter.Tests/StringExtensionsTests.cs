759ab137af9006a072fad3aa6728bd3a536eb909
Alex Kraevyy <clockworkalexorange@gmail.com>
2016-06-29 20:02:06 +0300

    Initial commit

---
+++ StringToIntConverter.Tests/StringExtensionsTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringToIntConverter.Tests
+{
+    [TestClass]
+    public class StringExtensionsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConvertToInt_StringIsEmpty_ThrowsException()
+        {
+            "".ConvertToInt();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConvertToInt_StringIsInvalidInStart_ThrowsException()
+        {
+            "asd1313".ConvertToInt();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ConvertToInt_StringIsInvalidInEnd_ThrowsException()
+        {
+            "123123asdsad".ConvertToInt();
+        }
+
+        [TestMethod]
+        public void ConvertToInt_123_Returns123()
+        {
+            Assert.AreEqual("123".ConvertToInt(), 123);
+        }
+
+        [TestMethod]
+        public void ConvertToInt_45_Returns45()
+        {
+            Assert.AreEqual("45".ConvertToInt(), 45);
+        }
+    }
+}
---
+++ StringToIntConverter/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace StringToIntConverter
+{
+    public static class StringExtensions
+    {
+        public static int ConvertToInt(this string input)
+        {
+            if (!IsStringValid(input))
+            {
+                throw new ArgumentException("String is not matches number");
+            }
+
+            var result = 0;
+            for (var i = 0; i < input.Length; i++)
+            {
+                var currentChar = ConvertCharToInt(input[i]);
+                var decimalNumber = GetDecimalNumber(input.Length, currentChar, i);
+
+                result += decimalNumber;
+            }
+            
+            return result;
+        }
+
+        private static int GetDecimalNumber(int @base, int numberLength, int offset)
+        {
+            return @base * GetDecimalBase((numberLength - 1) - offset);
+        }
+
+        private static bool IsStringValid(string input)
+        {
+            return Regex.IsMatch(input, @"^\d+$");
+        }
+
+        private static int GetDecimalBase(int exponent)
+        {
+            return (int) Math.Pow(10, exponent);
+        }
+
+        private static int ConvertCharToInt(char charNumber)
+        {
+            return charNumber - 48;
+        }
+    }
+}
